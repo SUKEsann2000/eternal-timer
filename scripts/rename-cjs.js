8152e428b04a0570d11b9bbfe228f35365855dfb
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2026-01-12 14:28:14 +0900

    rename all js files(cjs)

--- scripts/rename-cjs.js
+++ scripts/rename-cjs.js
@@ -1,2 +1,9 @@
 import fs from "fs";
-fs.renameSync("dist/cjs/index.js", "dist/cjs/index.cjs");
+const files = fs.readdirSync("dist/cjs/")
+
+for (const file of files) {
+    if (file.endsWith(".js")) {
+        const newName = file.slice(0, -3);
+        fs.renameSync(`dist/cjs/${file}`, `dist/cjs/${newName}.cjs`)
+    }
+}
