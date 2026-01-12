8bba5b898dfc72d31ef592267c5dea0f44320dff
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2026-01-12 14:28:48 +0900

    add eslint

---
+++ eslint/eslint.config.ts
@@ -0,0 +1,23 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  ...tseslint.configs.recommended.map((config: any) => ({
+    ...config,
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      ...config.languageOptions,
+      globals: globals.node,
+    },
+    rules: {
+      ...config.rules,
+
+      semi: ["error", "always"],
+
+      "comma-dangle": ["error", "always-multiline"],
+
+      indent: ["error", "tab", { SwitchCase: 1 }],
+    },
+  })),
+]);
