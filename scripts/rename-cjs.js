69d0bf3755a4b13a608bfbe20dc6195a2132ff2f
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2026-01-12 17:36:05 +0900

    rename file context

--- scripts/rename-cjs.js
+++ scripts/rename-cjs.js
@@ -1,9 +1,21 @@
 import fs from "fs";
-const files = fs.readdirSync("dist/cjs/")
+import path from "path";
+
+const dir = "dist/cjs/";
+const files = fs.readdirSync(dir);
 
 for (const file of files) {
-    if (file.endsWith(".js")) {
-        const newName = file.slice(0, -3);
-        fs.renameSync(`dist/cjs/${file}`, `dist/cjs/${newName}.cjs`)
-    }
+	if (file.endsWith(".js")) {
+		const oldPath = path.join(dir, file);
+		const newFileName = file.slice(0, -3) + ".cjs";
+		const newPath = path.join(dir, newFileName);
+
+		fs.renameSync(oldPath, newPath);
+
+		let content = fs.readFileSync(newPath, "utf-8");
+    
+		content = content.replace(/(require\(['"]\.\/[^'"]+)\.js(['"]\))/g, "$1.cjs$2");
+		content = content.replace(/(from ['"][^'"]+)\.js(['"])/g, "$1.cjs$2");
+		fs.writeFileSync(newPath, content, "utf-8");
+	}
 }
