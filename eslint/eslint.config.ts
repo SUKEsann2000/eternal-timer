ab690b58f5405d86e2328f0d3e221d60f882cc81
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2026-01-12 17:35:41 +0900

    lint: include scripts and test code

--- eslint/eslint.config.ts
+++ eslint/eslint.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   ...tseslint.configs.recommended.map((config: any) => ({
     ...config,
-    files: ["src/**/*.ts"],
+    files: ["src/**/*.ts", "test/**/*.{mjs,js}", "scripts/**/*.js"],
     languageOptions: {
       ...config.languageOptions,
       globals: globals.node,
