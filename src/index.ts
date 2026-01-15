70da30b3053ea37e65c4881c286d4ed6fc01f7b2
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2026-01-15 21:13:35 +0900

    add jsonl process

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ export type Timer = {
     id: string,
     start: number,
     stop: number,
-	title: string,
-	description: string
+	title?: string,
+	description?: string
 }
 
-async function checkTimerfileSyntax(fileData: string): Promise<void> {
+async function checkTimerfileSyntax(fileData: string, isJSONLines: boolean): Promise<void> {
 	const throwing = () => {
 		throw new Error(`Timer file's syntax is wrong`);
 	};
@@ -19,12 +19,23 @@ async function checkTimerfileSyntax(fileData: string): Promise<void> {
 		.split('\n')
 		.map(l => l.trim())
 		.filter(l => l !== "");
-	for (const timerData of timersData) {
-		const timerArray: string[] = timerData.split(/\s+/);
-		if (timerArray.length !== 3) throwing();
-		if (timerArray[0]?.length !== 36) throwing();
-		if (timerArray[1]!.trim() === "") throwing();
-		if (timerArray[2]!.trim() === "") throwing();
+	if (isJSONLines) {
+		for (const timerData of timersData) {
+			const parsed: Timer = JSON.parse(timerData);
+			if (!parsed.id || typeof parsed.id !== "string" || parsed.id.length !== 36) throwing();
+			if (!parsed.start || typeof parsed.start !== "number" || parsed.start.toString().trim() === "") throwing();
+			if (!parsed.stop || typeof parsed.stop !== "number" || parsed.stop.toString().trim() === "") throwing();
+			if (parsed.title && typeof parsed.title !== "string") throwing();
+			if (parsed.description && typeof parsed.description !== "string") throwing();
+		}
+	} else {
+		for (const timerData of timersData) {
+			const timerArray: string[] = timerData.split(/\s+/);
+			if (timerArray.length !== 3) throwing();
+			if (timerArray[0]?.length !== 36) throwing();
+			if (timerArray[1]!.trim() === "") throwing();
+			if (timerArray[2]!.trim() === "") throwing();
+		}
 	}
 	return;
 }
@@ -73,10 +84,15 @@ export class TimersManager {
      * const newTimer = await manager.createTimer(5000);
      * // newTimer will be id of the timer
      */
-	public async createTimer(length: number): Promise<string> {
+	public async createTimer(length: number, title?: string, description?: string): Promise<string> {
 		try {
 			if (length < 0) {
 				throw new Error(`Invailed length: ${length}`);
+			}
+			if (!this.isJSONLines) {
+				if (title || description) {
+					// out warn
+				}
 			}
 
 			length = Math.trunc(length);
@@ -84,7 +100,19 @@ export class TimersManager {
 			// uuid, start, end
 			const id = uuidv4();
 			const now = Date.now();
-			const newTimerData = `${id} ${now.toString()} ${(now + length).toString()}`;
+			let newTimerData: string;
+			if (this.isJSONLines) {
+				const json: Timer = {
+					id,
+					start: now,
+					stop: (now + length),
+					title,
+					description,
+				};
+				newTimerData = JSON.stringify(json, null, 0);
+			} else {
+				newTimerData = `${id} ${now.toString()} ${(now + length).toString()}`;
+			}
 			await fs.promises.appendFile(this.timerfiledir, newTimerData + "\n");
 			return id;
 		} catch (e) {
@@ -104,22 +132,43 @@ export class TimersManager {
 	public async removeTimer(id: string): Promise<void> {
 		try {
 			const timersRaw: string = await fs.promises.readFile(this.timerfiledir, "utf-8");
-			const timersData: string[] = timersRaw.split(/\r?\n/);
-            
+			
 			let newTimersData: string = "";
-			let found = false;
+			if (this.isJSONLines) {
+				const timersData: Timer[] = timersRaw
+					.split(/\r?\n/)
+					.filter(t => t.trim())
+					.map(line => JSON.parse(line) as Timer);
 
-			for (const timerData of timersData) {
-				if (!timerData.trim()) continue;
-				const [timerId] = timerData.split(" ");
-				if (timerId === id) {
-					found = true;
-					continue;
+				let found = false;
+				for (const timerData of timersData) {
+					if (!timerData) continue;
+
+					if (timerData.id === id) {
+						found = true;
+						continue;
+					}
+					newTimersData += `${JSON.stringify(timerData, null, 0)}\n`;
 				}
-				newTimersData += timerData + "\n";
-			}            
-			if (!found) {
-				throw new Error(`Timer with id ${id} not found`);
+				if (!found) {
+					throw new Error(`Timer with id ${id} not found`);
+				}
+			} else {
+				const timersData: string[] = timersRaw.split(/\r?\n/);
+				let found = false;
+
+				for (const timerData of timersData) {
+					if (!timerData.trim()) continue;
+					const [timerId] = timerData.split(" ");
+					if (timerId === id) {
+						found = true;
+						continue;
+					}
+					newTimersData += timerData + "\n";
+				}            
+				if (!found) {
+					throw new Error(`Timer with id ${id} not found`);
+				}
 			}
 			await fs.promises.writeFile(this.timerfiledir, newTimersData, "utf-8");
 			return;
@@ -144,19 +193,32 @@ export class TimersManager {
 		try {
 			return setInterval(async () => {
 				const timersDataRaw: string = await fs.promises.readFile(this.timerfiledir, "utf-8");
-				const timersData: string[] = timersDataRaw.split(/\r?\n/);
 				const timersSet = new Set<Timer>();
-				await checkTimerfileSyntax(timersDataRaw);
+				await checkTimerfileSyntax(timersDataRaw, this.isJSONLines);
 
-				for (const timerData of timersData) {
-					if (!timerData.trim()) continue;
-					const [id, startStr, stopStr] = timerData.split(" ");
-					timersSet.add({
+				if (this.isJSONLines) {
+					const timersData: Timer[] = timersDataRaw
+					  .split(/\r?\n/)
+					  .filter(line => line.trim())
+					  .map(line => JSON.parse(line) as Timer);
+				  
+					for (const timer of timersData) {
+					  timersSet.add(timer);
+					}
+				} else {
+					const timersData: string[] = timersDataRaw.split(/\r?\n/);
+					
+					for (const timerData of timersData) {
+						if (!timerData.trim()) continue;
+						const [id, startStr, stopStr] = timerData.split(" ");
+						timersSet.add({
 						id: id!,
 						start: Number(startStr!),
 						stop: Number(stopStr!),
-					});
+						});
+					}
 				}
+				  
 
 				const now = Date.now();
 				for (const timer of timersSet) {
@@ -184,19 +246,28 @@ export class TimersManager {
 	public async showTimers(): Promise<Timer[]> {
 		try {
 			const timersRaw: string = await fs.promises.readFile(this.timerfiledir, "utf-8");
-			const timersData: string[] = timersRaw.split(/\r?\n/);
+			if (this.isJSONLines) {
+				const timersData: Timer[] = timersRaw
+					.split(/\r?\n/)
+					.filter(t => t.trim())
+					.map(line => JSON.parse(line) as Timer);
+				
+				return timersData;
+			} else {
+				const timersData: string[] = timersRaw.split(/\r?\n/);
 
-			const timersJSON: Timer[] = [];
-			for (const timerData of timersData) {
-				const splitedTimerData = timerData.split(" ");
-				if (!timerData.trim()) continue;
-				timersJSON.push({
-					id: splitedTimerData[0]!,
-					start: Number(splitedTimerData[1]!),
-					stop: Number(splitedTimerData[2]!),
-				});
+				const timersJSON: Timer[] = [];
+				for (const timerData of timersData) {
+					const splitedTimerData = timerData.split(" ");
+					if (!timerData.trim()) continue;
+					timersJSON.push({
+						id: splitedTimerData[0]!,
+						start: Number(splitedTimerData[1]!),
+						stop: Number(splitedTimerData[2]!),
+					});
+				}
+				return timersJSON;
 			}
-			return timersJSON;
 		} catch (e) {
 			throw new Error(`Error when showing timers: ${e}`);
 		}
