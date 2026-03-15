51379eb7466332b05b17cc8d0fbe8dbe16b55d26
SUKEsann2000 <129367282+SUKEsann2000@users.noreply.github.com>
2026-03-15 18:17:26 +0900

    udpate build script using package.json to change type(module or commonjs) removing rename-cjs.js and add move-package-json.js

---
+++ scripts/move-package-json.js
@@ -0,0 +1,18 @@
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve(path.dirname(__filename), "..");
+const CJS_PACKAGE = path.join(__dirname, "./package.jsons/cjs_package.json");
+const ESM_PACKAGE = path.join(__dirname, "./package.jsons/esm_package.json");
+
+async function move() {
+    // cjs
+    await fs.copyFile(CJS_PACKAGE, path.join(__dirname, "./dist/cjs/package.json"));
+
+    // module
+    await fs.copyFile(ESM_PACKAGE, path.join(__dirname, "./dist/esm/package.json"));
+}
+
+await move();
